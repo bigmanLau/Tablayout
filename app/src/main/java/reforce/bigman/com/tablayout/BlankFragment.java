28a33016758e3d8f33275ffe5e7ab35c1c158445
lgr <344760694@qq.com>
2018-08-28 13:55:58 +0800

    提交第一个版本
    提交第一个版本

---
+++ app/src/main/java/reforce/bigman/com/tablayout/BlankFragment.java
@@ -0,0 +1,31 @@
+package reforce.bigman.com.tablayout;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+@SuppressLint("ValidFragment")
+public class BlankFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_blank, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+    public static BlankFragment newInstance() {
+        BlankFragment fragment = new BlankFragment();
+        return fragment;
+    }
+}
